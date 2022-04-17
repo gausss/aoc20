c9d8810eb36de60ecf64dc7bf7f4b8c6ec9c8786
gausss <gausssven@gmail.com>
2022-04-17 15:30:30 +0200

    Initial projet setup

---
+++ src/test/kotlin/Ex1Test.kt
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+internal class Ex1Test {
+
+    @BeforeEach
+    fun setUp() {
+    }
+
+    @Test
+    fun main() {
+    }
+}
